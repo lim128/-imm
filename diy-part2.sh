eae93595585b80de3b9fbf3f579465ab1fd2b55a
lim128 <32699966+lim128@users.noreply.github.com>
2025-07-12 09:16:16 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -21,7 +21,7 @@
 sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
 rm -rf feeds/{luci,packages}/{applications,lang,net}/{luci-app-daed,luci-app-homeproxy,luci-app-openclash,luci-app-passwall,golang,chinadns-ng,daed,dns2socks-rust,dns2socks,dns2tcp,geoview,hysteria,ipt2socks,microsocks,mosdns,naiveproxy,nikki,pdnsd,redsocks2,shadow-tls,simple-obfs,sing-box,tcping,trojan-plus,trojan,tuic-client,v2ray-core,v2ray-geodata,v2ray-plugin,xray-core,xray-plugin}
 git clone -b 24.x --single-branch https://github.com/sbwml/packages_lang_golang.git feeds/packages/lang/golang
-git clone -b v5 --single-branch https://github.com/sbwml/openwrt_helloworld.git package/helloworld
-git clone -b v5 --single-branch https://github.com/sbwml/luci-app-mosdns.git package/luci-app-mosdns
+#git clone -b v5 --single-branch https://github.com/sbwml/openwrt_helloworld.git package/helloworld
+#git clone -b v5 --single-branch https://github.com/sbwml/luci-app-mosdns.git package/luci-app-mosdns
 #git clone -b master --single-branch https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
-git clone -b js --single-branch https://github.com/sirpdboy/luci-app-poweroffdevice.git package/luci-app-poweroffdevice
+#git clone -b js --single-branch https://github.com/sirpdboy/luci-app-poweroffdevice.git package/luci-app-poweroffdevice
