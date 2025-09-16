3ffb4b82ccec2cc579db4a02a2c9123738f7d1fe
lim128 <32699966+lim128@users.noreply.github.com>
2025-09-16 15:03:23 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,4 +18,3 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 sed -i '1i src-git helloworld https://github.com/sbwml/openwrt_helloworld.git;v5' feeds.conf.default
 sed -i '2i src-git mosdns https://github.com/sbwml/luci-app-mosdns.git;v5' feeds.conf.default
-#sed -i '3i src-git poweroffdevice https://github.com/sirpdboy/luci-app-poweroffdevice.git;js' feeds.conf.default
